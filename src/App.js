8435089a8c49499d3b28b5f9a47215540c9ef4fd
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-23 15:21:11 +0600

    Added image components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Paragraph from "./components/Paragraph/Paragraph.js";
+import Paragraph from './components/Paragraph/Paragraph.js';
+import Image from './components/Image/Image.js';
+import koalaImage from './assets/koala.jpg';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
 
-      <Paragraph size="14px" color="red">This is sample paragraph</Paragraph>
+      <Paragraph size="14px" color="purple">This is sample paragraph</Paragraph>
+      
+      <Paragraph size=".5rem" color="red">This is small paragraph</Paragraph>
+
+
+      <Image url={koalaImage} width="150px" height="100px"/>
     </div>
   );
 }
---
+++ src/components/Image/Image.js
@@ -0,0 +1,10 @@
+function Image(props) {
+
+  const style = {
+    width: props.width,
+    height: props.height,
+  }
+  return <img style={style} src={props.url}/>
+}
+
+export default Image;
