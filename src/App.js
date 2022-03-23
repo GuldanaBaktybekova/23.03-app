ed5db14e19084f83c5abd76d646624e9e6b56626
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-23 21:35:46 +0600

    Added Button components

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import koalaImage from "./assets/koala.jpg";
 
 import Alert from "./components/Alert/Alert";
 
+import Button from "./components/Button/Button";
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
 
       <Alert primary> A simple primary alert—check it out!</Alert>
-      <Alert seconda ry>
-        {" "}
-        A simple secondary alert—check it out!
-      </Alert>
+      <Alert secondary>A simple secondary alert—check it out!</Alert>
       <Alert success>A simple success alert—check it out!</Alert>
       <Alert danger>A simple danger alert—check it out!</Alert>
       <Alert warning>A simple warning alert—check it out!</Alert>
@@ -22,18 +22,14 @@ function App() {
       <Alert dark>A simple dark alert—check it out!</Alert>
 
       <Button primary>Primary</Button>
-
-      <Button primary>Primary</Button>
-
-      <Button primary>Primary</Button>
-
-      <Button primary>Primary</Button>
-
-      <Button primary>Primary</Button>
-
-      <Button primary>Primary</Button>
-
-      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+      <Button success>Success</Button>
+      <Button danger>Danger</Button>
+      <Button warning>Warning</Button>
+      <Button info>Info</Button>
+      <Button light>Light</Button>
+      <Button dark>Dark</Button>
+      <Button link>Link</Button>
 
       {/* <Paragraph size="14px" color="purple">This is sample paragraph</Paragraph>
       
