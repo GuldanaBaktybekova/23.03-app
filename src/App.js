18b259301a9a2fb644baecebdba8615bc3b1729d
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-23 21:16:20 +0600

    Added alert style

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import Paragraph from './components/Paragraph/Paragraph.js';
-import Image from './components/Image/Image.js';
-import koalaImage from './assets/koala.jpg';
+import Paragraph from "./components/Paragraph/Paragraph.js";
+import Image from "./components/Image/Image.js";
+import koalaImage from "./assets/koala.jpg";
 
-import Alert from './components/Alert/Alert';
+import Alert from "./components/Alert/Alert";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
 
-      
-      
       <Alert primary> A simple primary alert—check it out!</Alert>
-      <Alert secondery> A simple secondery alert—check it out!</Alert>
+      <Alert seconda ry>
+        {" "}
+        A simple secondary alert—check it out!
+      </Alert>
       <Alert success>A simple success alert—check it out!</Alert>
       <Alert danger>A simple danger alert—check it out!</Alert>
       <Alert warning>A simple warning alert—check it out!</Alert>
       <Alert info>A simple info alert—check it out!</Alert>
       <Alert light>A simple light alert—check it out!</Alert>
       <Alert dark>A simple dark alert—check it out!</Alert>
-  
 
-      <Paragraph size="14px" color="purple">This is sample paragraph</Paragraph>
+      <Button primary>Primary</Button>
+
+      <Button primary>Primary</Button>
+
+      <Button primary>Primary</Button>
+
+      <Button primary>Primary</Button>
+
+      <Button primary>Primary</Button>
+
+      <Button primary>Primary</Button>
+
+      <Button primary>Primary</Button>
+
+      {/* <Paragraph size="14px" color="purple">This is sample paragraph</Paragraph>
       
       <Paragraph size=".5rem" color="red">This is small paragraph</Paragraph>
 
 
-      <Image url={koalaImage} width="150px" height="100px"/>
+      <Image url={koalaImage} width="150px" height="100px"/> */}
     </div>
   );
 }
