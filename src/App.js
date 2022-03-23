c19f04a93a3e25e171d9f0560a8df6b357fe060b
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-23 21:08:39 +0600

    Added alert classname

--- src/App.js
+++ src/App.js
@@ -10,8 +10,16 @@ function App() {
       <h1>Hello world</h1>
 
       
-      <Alert success>This is a sample alert</Alert>
-      <Alert danger>This is a sample alert</Alert>
+      
+      <Alert primary> A simple primary alert—check it out!</Alert>
+      <Alert secondery> A simple secondery alert—check it out!</Alert>
+      <Alert success>A simple success alert—check it out!</Alert>
+      <Alert danger>A simple danger alert—check it out!</Alert>
+      <Alert warning>A simple warning alert—check it out!</Alert>
+      <Alert info>A simple info alert—check it out!</Alert>
+      <Alert light>A simple light alert—check it out!</Alert>
+      <Alert dark>A simple dark alert—check it out!</Alert>
+  
 
       <Paragraph size="14px" color="purple">This is sample paragraph</Paragraph>
       
