e508c0689a0803f250ead73de52e4b04b17c77ec
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-23 14:56:57 +0600

    Added paragraph components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Paragraph from "./components/Paragraph/Paragraph.js";
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
 
+      <Paragraph size="14px" color="red">This is sample paragraph</Paragraph>
     </div>
   );
 }
