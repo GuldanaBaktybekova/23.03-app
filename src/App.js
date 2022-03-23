0bb1a49212362d5b42c235fed07e37a79adfd8bf
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-23 20:58:18 +0600

    Added alert components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import Paragraph from './components/Paragraph/Paragraph.js';
 import Image from './components/Image/Image.js';
 import koalaImage from './assets/koala.jpg';
 
+import Alert from './components/Alert/Alert';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+
+      
+      <Alert success>This is a sample alert</Alert>
+      <Alert danger>This is a sample alert</Alert>
 
       <Paragraph size="14px" color="purple">This is sample paragraph</Paragraph>
       
--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,5 +1,15 @@
-import "./Alert.css"
+import "./Alert.css";
+
 function Alert(props) {
-  return <div>{props.children}</div>
+  const className = ["Alert"];
+  if (props.success) {
+    className.push('success');
+  }
+  if (props.danger) {
+    className.push('danger');
+  }
+
+  return <div className={className.join(' ')}>{props.children}</div>;
 }
-export default Alert;
+
+export default Alert; 
